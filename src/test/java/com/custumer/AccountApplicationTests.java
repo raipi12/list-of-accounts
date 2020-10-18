495d5b50b78bae64800f844ddca1e0b88f0d9a16
Panainte Dumitras <dimapanainte2@gmail.com>
2020-10-18 16:02:27 +0300

    The last commit

--- src/test/java/com/custumer/AccountApplicationTests.java
+++ src/test/java/com/custumer/AccountApplicationTests.java
@@ -3,11 +3,15 @@ package com.custumer;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class AccountApplicationTests {
+public class AccountApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
 	}
 
 }
